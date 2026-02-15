b9c991f68f78e7146335d0129404141cb1a71923
Boomcrasc078 <mose.g07@gmail.com>
2026-02-15 16:00:27 +0100

    Added Among us irl start

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import { createServer } from 'node:http';
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
-import { Server } from 'socket.io';
+import express from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
@@ -10,22 +10,20 @@ const io = new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'public/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "public/index.html"));
 });
 
-io.on('connection', (socket) => {
-console.log(`Socket ${socket.id} has been connected`)
-})
+io.on("connection", (socket) => {
+  console.log(`Socket ${socket.id} has been connected`);
+});
 
-function update() {
-
-}
+function update() {}
 
 setInterval(update, 15);
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  console.log("server running at http://localhost:3000");
 });
